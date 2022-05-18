97d8d96c5cb7b2c42cd0111c0100765dcea28d59
jerinjames707 <jerinjames707@gmail.com>
2022-05-18 13:38:46 +0530

    socialmedia-addpost cmplt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Addpost from './components/Addpost';
 
 function App() {
   return (
     <div>
-
+<Addpost/>
 
     </div>
   );
