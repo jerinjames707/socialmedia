934d8886871f5228077c2184090e99fa0d76ad8b
jerinjames707 <jerinjames707@gmail.com>
2022-05-18 13:52:36 +0530

    socialmedia-navbar cmplt

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addpost from './components/Addpost';
 import Searchpost from './components/Searchpost';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Searchpost/>
+      <BrowserRouter>
+     <Routes>
+      <Route path="/" exact element={<Addpost/>}/>
+      <Route path="/search" exact element={<Searchpost/>}/>
+      
+     
+     </Routes>
+     </BrowserRouter>
+    
 
     </div>
   );
